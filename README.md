988107cf6f9658aff0afa511e582129b15cb76e3
EMish <Liza.s.mishina@gmail.com>
2020-09-20 17:43:39 -0700

    Challange

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mission-to-Mars
+## Purpose
+The Purpose of this program is to create your own website using information already on the internet. We scrapped data off of various websites mostly NASA and then using that data we formed our own website using Python and HTML.
--- scraping.py
+++ scraping.py
@@ -17,6 +17,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": mars_hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
     browser.quit()
@@ -97,6 +98,41 @@ def mars_facts():
     
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def  mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    beg_url = "https://astrogeology.usgs.gov"
+    
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    soups_1 = soup(html, 'html.parser')
+    articles = soups_1.find_all('div', class_='item')
+    
+    for article in articles:
+        h3= article.find('h3').text
+        link = article.find('a')
+        href = link['href']
+        pg_url = (beg_url + href)
+        
+        browser.visit(pg_url)
+        html = browser.html
+        soups = soup(html, 'html.parser')
+        imgs = soups.find_all('img', class_="wide-image")
+        for img in imgs:
+            img_end_url = img["src"]
+            
+            full_img_url = (beg_url + img_end_url)
+        
+            hemispheres = {'img_url': full_img_url, 'title' : h3}
+        
+            hemisphere_image_urls.append(hemispheres)
+
+    return hemisphere_image_urls
 
 
 if __name__ == "__main__":
